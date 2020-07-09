92364797c2c880f82a96fce3cb6a3d3f7ca7490a
Scott White <spwhite1337@gmail.com>
2020-07-09 16:33:13 -0400

    drop cols

--- iowa_football/curate.py
+++ iowa_football/curate.py
@@ -26,24 +26,28 @@ def curate_data():
             apply(lambda pca: int(pca.split('-')[0]) if isinstance(pca, str) else pca)
         df_stats[home_away + '_passAttempts'] = df_stats[home_away + '_completionAttempts'].\
             apply(lambda pca: int(pca.split('-')[1]) if isinstance(pca, str) else pca)
+        df_stats = df_stats.drop(home_away + '_completionAttempts')
 
         # Third Downs
         df_stats[home_away + '_thirdDownCompletions'] = df_stats[home_away + '_thirdDownEff'].\
             apply(lambda tde: int(tde.split('-')[0]) if isinstance(tde, str) else tde)
         df_stats[home_away + '_thirdDownAttempts'] = df_stats[home_away + '_thirdDownEff'].\
             apply(lambda tde: int(tde.split('-')[1]) if isinstance(tde, str) else tde)
+        df_stats = df_stats.drop(home_away + '_thirdDownEff')
 
         # Fourth Downs
         df_stats[home_away + '_fourthDownCompletions'] = df_stats[home_away + '_fourthDownEff'].\
             apply(lambda fde: int(fde.split('-')[0]) if isinstance(fde, str) else fde)
         df_stats[home_away + '_fourthDownAttempts'] = df_stats[home_away + '_fourthDownEff'].\
             apply(lambda fde: int(fde.split('-')[1]) if isinstance(fde, str) else fde)
+        df_stats = df_stats.drop(home_away + '_fourthDownEff')
 
         # Penalties
         df_stats[home_away + '_numPenalties'] = df_stats[home_away + '_totalPenaltiesYards'].\
             apply(lambda tpy: int(tpy.split('-')[0]) if isinstance(tpy, str) else tpy)
         df_stats[home_away + '_penaltyYards'] = df_stats[home_away + '_totalPenaltiesYards'].\
             apply(lambda tpy: int(tpy.split('-')[1]) if isinstance(tpy, str) else tpy)
+        df_stats = df_stats.drop(home_away + '_totalPenaltiesYards')
 
         # Possession Time
         df_stats[home_away + '_possessionTime'] = df_stats[home_away + '_possessionTime'].\
