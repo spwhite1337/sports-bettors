29fc4d8342497bf5a82f51b00b9415f8433dc222
Scott White <spwhite1337@gmail.com>
2020-07-14 11:23:39 -0400

    move betting aids to config

--- sports_bettors/experiments.py
+++ sports_bettors/experiments.py
@@ -1,12 +1,8 @@
 import os
 import pickle
 import argparse
-from sports_bettors.utils.college_football.models import CollegeFootballBettingAid
-from sports_bettors.utils.nfl.models import NFLBettingAid
 
-from config import ROOT_DIR, logger, version
-
-betting_aids = {'nfl': NFLBettingAid, 'college_football': CollegeFootballBettingAid}
+from config import ROOT_DIR, logger, version, betting_aids
 
 
 def execute_experiments(league: str, overwrite: bool = False):
