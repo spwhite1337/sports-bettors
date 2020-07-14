55f351f30d1ea224020ca86e2c1182a6ff7e6e88
Scott White <spwhite1337@gmail.com>
2020-07-14 11:19:37 -0400

    name change

--- sports_bettors/curate.py
+++ sports_bettors/curate.py
@@ -1,7 +1,7 @@
 import argparse
 
-from college_football.utils.college_football.curate import curate_college
-from college_football.utils.nfl.curate import curate_nfl
+from sports_bettors.utils.college_football.curate import curate_college
+from sports_bettors.utils.nfl.curate import curate_nfl
 
 
 def curate_data():
@@ -9,10 +9,10 @@ def curate_data():
     Curate downloaded data into a set for modeling / plotting.
     """
     parser = argparse.ArgumentParser(prog='Football Curated')
-    parser.add_argument('--league', default='sports_bettors')
+    parser.add_argument('--league', required=True)
     args = parser.parse_args()
 
-    if args.league == 'sports_bettors':
+    if args.league == 'college_football':
         curate_college()
     elif args.league == 'nfl':
         curate_nfl()
