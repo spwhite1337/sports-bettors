f77a81f4043f09069019ad79284b17bb2fdaaa06
Scott White <spwhite1337@gmail.com>
2020-07-17 17:41:05 -0400

    Bug in entrypoint

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         'sb_curate = sports_bettors.curate:curate_data',
         'sb_run_experiments = sports_bettors.experiments:run_experiments',
         'sb_predict = sports_bettors.api:api_cli',
-        'sb_generate_predictors = sports_bettors.apicreate_predictor_sets',
+        'sb_generate_predictors = sports_bettors.api:create_predictor_sets',
         'sb_upload = sports_bettors.upload:upload'
     ]},
     install_requires=[
