78aef49db5fcac96b4c6e44afd50a6d0cc737c6a
Scott White <spwhite1337@gmail.com>
2020-07-18 07:48:07 -0400

    rename to display output

--- sports_bettors/api.py
+++ sports_bettors/api.py
@@ -31,7 +31,7 @@ class SportsPredictor(object):
         with open(os.path.join(self.load_dir, self.league, 'predictor_set_{}.pkl'.format(self.version)), 'rb') as fp:
             self.predictors = pickle.load(fp)
 
-    def predict(self, random_effect: str, feature_set: str, inputs: dict, display_results: bool = False) -> dict:
+    def predict(self, random_effect: str, feature_set: str, inputs: dict, display_output: bool = False) -> dict:
         """
         Predict with all models in predictor set, imputing missing values to the mean of the training set
         """
@@ -51,7 +51,7 @@ class SportsPredictor(object):
                 output = self.predictors[key](inputs)
                 outputs[(random_effect, feature_set, response)] = output
 
-        if display_results:
+        if display_output:
             logger.info('Output: \n {}'.format(pformat(outputs)))
 
         return outputs
