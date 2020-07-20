9e8aab06ce652ec9fb72f4ccb56ee4972bf581ed
Scott White <spwhite1337@gmail.com>
2020-07-20 08:33:40 -0400

    put upload in a func

--- sports_bettors/upload.py
+++ sports_bettors/upload.py
@@ -5,32 +5,33 @@ import argparse
 from config import Config, logger
 
 
-parser = argparse.ArgumentParser(prog='Upload card classifier')
-parser.add_argument('--windows', action='store_true')
-parser.add_argument('--skipdata', action='store_true')
-parser.add_argument('--skipresults', action='store_true')
-parser.add_argument('--dryrun', action='store_true')
-args = parser.parse_args()
+def upload():
+    parser = argparse.ArgumentParser(prog='Upload card classifier')
+    parser.add_argument('--windows', action='store_true')
+    parser.add_argument('--skipdata', action='store_true')
+    parser.add_argument('--skipresults', action='store_true')
+    parser.add_argument('--dryrun', action='store_true')
+    args = parser.parse_args()
 
-# General commands
-sync_base = 'aws s3 sync '
-dryrun_arg = ' --dryrun'
-results_sync = '{} {}'.format(Config.RESULTS_DIR, Config.CLOUD_RESULTS)
-data_sync = '{} {}'.format(Config.DATA_DIR, Config.CLOUD_DATA)
+    # General commands
+    sync_base = 'aws s3 sync '
+    dryrun_arg = ' --dryrun'
+    results_sync = '{} {}'.format(Config.RESULTS_DIR, Config.CLOUD_RESULTS)
+    data_sync = '{} {}'.format(Config.DATA_DIR, Config.CLOUD_DATA)
 
-include_flag = " --exclude '*/.gitignore'"
-if args.windows:
-    include_flag = re.sub("'", "", include_flag)
+    include_flag = " --exclude '*/.gitignore'"
+    if args.windows:
+        include_flag = re.sub("'", "", include_flag)
 
-if not args.skipdata:
-    logger.info('Uploading Data')
-    sb_sync = sync_base + data_sync + include_flag
-    sb_sync += dryrun_arg if args.dryrun else ''
-    logger.info(sb_sync)
-    os.system(sb_sync)
-if not args.skipresults:
-    logger.info('Uploading Results')
-    sb_sync = sync_base + results_sync + include_flag
-    sb_sync += dryrun_arg if args.dryrun else ''
-    logger.info(sb_sync)
-    os.system(sb_sync)
+    if not args.skipdata:
+        logger.info('Uploading Data')
+        sb_sync = sync_base + data_sync + include_flag
+        sb_sync += dryrun_arg if args.dryrun else ''
+        logger.info(sb_sync)
+        os.system(sb_sync)
+    if not args.skipresults:
+        logger.info('Uploading Results')
+        sb_sync = sync_base + results_sync + include_flag
+        sb_sync += dryrun_arg if args.dryrun else ''
+        logger.info(sb_sync)
+        os.system(sb_sync)
