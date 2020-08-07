1f5e869f268ca8150f7f3a224a0e57be1c640286
Scott White <spwhite1337@gmail.com>
2020-08-06 21:28:03 -0400

    Add pseudo code

--- sports_bettors/dash.py
+++ sports_bettors/dash.py
@@ -10,4 +10,20 @@ def add_sb_dash(server, routes_pathname_prefix: str = '/api/dash/sportsbettors/'
     )
     dashapp.layout = html.H1('Hi From Dash (sports bettors)')
 
+    # On load; calculate default data set. The change in these parameters will be triggered by a button:
+    #   League: college-football;
+    #   Conditions: PointsScored;
+    #   Teams: Iowa, Wisconsin, Michigan
+    #   Output: Probability or Log-odds (We might be able to return this and toggle it reactively)
+
+    # Store data in 3 hidden divs
+    # for team in teams:
+    #   for total_points_in_game in range(10, 100):
+    #       calculate probability of winning with error bars (Main Figure) for each team
+    #           Fields: Team, TotalPoints, LB, E, UB
+    #       calculate probability of winning with error bars by 0 -> 21 for each team (Figure that depends on slice)
+    #           Fields: Team, TotalPoints, WinMargin, LB, E, UB
+    #       calculate probability of losing with error bars by 0 -> 21 for each team (Figure that depends on slice)
+    #           Fields: Team, TotalPoints, LossMargin, LB, E, UB
+
     return dashapp.server
