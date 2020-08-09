eb4533aca998db2f3be67c3e506851ee44a74d40
Scott White <spwhite1337@gmail.com>
2020-08-09 12:47:16 -0400

    Comments, typehints, docstrings

--- sports_bettors/dashboard/callbacks.py
+++ sports_bettors/dashboard/callbacks.py
@@ -9,6 +9,9 @@ from config import Config
 
 
 class ConfigCallbacks(object):
+    """
+    Configure the layout to create inputs and display html elements
+    """
     @staticmethod
     def dropdowns(league: str):
         team_opts = params[Config.version]['team-opts'][league]
@@ -16,19 +19,26 @@ class ConfigCallbacks(object):
         return team_opts, utils['show'], team_opts, utils['show'], feature_set_opts, utils['show']
 
     @staticmethod
-    def variables(league, feature_set):
+    def variables(league: str, feature_set: str):
+        """
+        Populate and show a drop down that selects the "variable" to span for results
+        """
         if (league is None) or (feature_set is None):
             return [], utils['no_show']
         variable_opts = params[Config.version]['variable-opts'][league][feature_set]
         return variable_opts, utils['show']
 
     @staticmethod
-    def parameters(feature_set, variable, league):
+    def parameters(feature_set: str, variable: str, league: str):
+        """
+        Populate and show inputs for defining parameters of the input model
+        """
         if (league is None) or (feature_set is None) or (variable is None):
             return [None] * 4 + [None] * 4 + [utils['no_show']] * 4
-        # Get parameters except for variable and fill with Nones
-        parameters = [p['label'] for p in params[Config.version]['variable-opts'][league][feature_set]
-                      if p['value'] != variable]
+        # Get parameters except for variable
+        parameters = [
+            p['label'] for p in params[Config.version]['variable-opts'][league][feature_set] if p['value'] != variable
+        ]
         # Fill displays and Nones
         displays = [utils['show']] * len(parameters) + [utils['no_show']] * (4 - len(parameters))
         parameters += [None] * (4 - len(parameters))
@@ -37,13 +47,22 @@ class ConfigCallbacks(object):
 
 
 class DataCallbacks(object):
+    """
+    Generate the historical and results-based data to display in the dashboard
+    """
     @staticmethod
-    def history(league, team, opponent):
+    def history(league: str, team: str, opponent: str):
+        """
+        Load data for historical matchups
+        """
         df, x_opts, y_opts = history_populate(league, team, opponent)
         return df.to_json(), x_opts, y_opts
 
     @staticmethod
-    def results(league, feature_set, team, opponent, variable, *parameters):
+    def results(league: str, feature_set: str, team: str, opponent: str, variable: str, *parameters):
+        """
+        Calculate probabilities
+        """
         # Drop nones
         parameters = [p for p in parameters if p]
         # Convert to dictionary
@@ -62,8 +81,14 @@ class DataCallbacks(object):
 
 
 class PlotCallbacks(object):
+    """
+    Generate plotly figures from history and results
+    """
     @staticmethod
-    def history(df, x, y):
+    def history(df, x: str, y: str):
+        """
+        Plot historical data
+        """
         df = pd.read_json(df, orient='records')
         if df.shape[0] == 0:
             return utils['empty_figure'], utils['no_show'], utils['no_show']
@@ -74,6 +99,9 @@ class PlotCallbacks(object):
 
     @staticmethod
     def results(df):
+        """
+        Plot results
+        """
         df = pd.read_json(df, orient='records')
         if df.shape[0] == 0:
             return utils['empty_figure'], utils['empty_figure']
