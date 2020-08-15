301bb7edc5b4db1d72cc0ba14c299beb32544d28
Scott White <spwhite1337@gmail.com>
2020-08-15 11:56:36 -0400

    Add gridlines

--- sports_bettors/dashboard/callbacks.py
+++ sports_bettors/dashboard/callbacks.py
@@ -113,6 +113,7 @@ class PlotCallbacks(object):
         x = df.columns[0] if not x else x
         y = df.columns[0] if not y else y
         fig = px.scatter(df, x=x, y=y, color='Winner', template='simple_white', title='Matchup Data')
+        fig.update_layout(xaxis_showgrid=True, yaxis_showgrid=True)
         return fig, utils['show'], utils['show'], utils['show']
 
     @staticmethod
@@ -128,6 +129,7 @@ class PlotCallbacks(object):
             df = df.sort_values(variable)
             fig = px.line(df, x=variable, y='Win', error_y='WinUB', error_y_minus='WinLB', template='simple_white',
                           title='Win Probability')
+            fig.update_layout(xaxis_showgrid=True, yaxis_showgrid=True)
             return fig, utils['show']
 
     @staticmethod
@@ -140,12 +142,13 @@ class PlotCallbacks(object):
             return utils['empty_figure'], utils['no_show']
         else:
             if variable_val is None:
-                return utils['empty_figure']
+                return utils['empty_figure'], utils['no_show']
             # Win margin figure
             variable_val = variable_val['points'][0]['x']
             df = df[df['variable_val'] == variable_val].sort_values('Margin')
             fig = px.line(df, x='Margin', y='Probability', error_y='Probability_UB', error_y_minus='Probability_LB',
                           color='Result', template='simple_white', title='Margins / Spreads')
+            fig.update_layout(xaxis_showgrid=True, yaxis_showgrid=True)
             return fig, utils['show']
 
     @staticmethod
@@ -164,4 +167,5 @@ class PlotCallbacks(object):
             df = df[df['variable_val'] == variable_val].sort_values('TotalPoints')
             fig = px.line(df, x='TotalPoints', y='Probability', error_y='Probability_UB',
                           error_y_minus='Probability_LB', template='simple_white', title='Total Points in Matchup')
+            fig.update_layout(xaxis_showgrid=True, yaxis_showgrid=True)
             return fig, utils['show']
