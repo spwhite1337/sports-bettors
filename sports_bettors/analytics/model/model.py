6a08aa8125d186c8c12801b20e2aead2544ba379
Scott White <spwhite1337@gmail.com>
2023-11-14 16:03:17 -0500

    features

--- sports_bettors/analytics/model/model.py
+++ sports_bettors/analytics/model/model.py
@@ -40,18 +40,18 @@ class Model(Data):
                     # Win-rate ATS / Over
                     'favorite_team_win_rate_ats',
                     'underdog_team_win_rate_ats',
-                    'favorite_team_margin_ats',
-                    'underdog_team_margin_ats',
+                    # 'favorite_team_margin_ats',
+                    # 'underdog_team_margin_ats',
 
                     # PF and PA
-                    'favorite_team_over_rate',
-                    'underdog_team_over_rate',
-                    'favorite_team_total_points_ats',
-                    'underdog_team_total_points_ats',
-                    # 'favorite_team_points_for',
-                    # 'underdog_team_points_for',
-                    # 'favorite_team_points_against',
-                    # 'underdog_team_points_against',
+                    # 'favorite_team_over_rate',
+                    # 'underdog_team_over_rate',
+                    # 'favorite_team_total_points_ats',
+                    # 'underdog_team_total_points_ats',
+                    'favorite_team_points_for',
+                    'underdog_team_points_for',
+                    'favorite_team_points_against',
+                    'underdog_team_points_against',
                 ]
             },
             'over': {
@@ -66,18 +66,18 @@ class Model(Data):
                     # Win-rate ATS
                     'favorite_team_win_rate_ats',
                     'underdog_team_win_rate_ats',
-                    'favorite_team_margin_ats',
-                    'underdog_team_margin_ats',
+                    # 'favorite_team_margin_ats',
+                    # 'underdog_team_margin_ats',
 
                     # PF and PA
-                    'favorite_team_over_rate',
-                    'underdog_team_over_rate',
-                    'favorite_team_total_points_ats',
-                    'underdog_team_total_points_ats',
-                    # 'favorite_team_points_for',
-                    # 'underdog_team_points_for',
-                    # 'favorite_team_points_against',
-                    # 'underdog_team_points_against',
+                    # 'favorite_team_over_rate',
+                    # 'underdog_team_over_rate',
+                    # 'favorite_team_total_points_ats',
+                    # 'underdog_team_total_points_ats',
+                    'favorite_team_points_for',
+                    'underdog_team_points_for',
+                    'favorite_team_points_against',
+                    'underdog_team_points_against',
                 ]
             }
         },
@@ -94,18 +94,18 @@ class Model(Data):
                     # Win-rate ATS
                     'favorite_team_win_rate_ats',
                     'underdog_team_win_rate_ats',
-                    'favorite_team_margin_ats',
-                    'underdog_team_margin_ats',
+                    # 'favorite_team_margin_ats',
+                    # 'underdog_team_margin_ats',
 
                     # PF and PA
-                    'favorite_team_over_rate',
-                    'underdog_team_over_rate',
-                    'favorite_team_total_points_ats',
-                    'underdog_team_total_points_ats',
-                    # 'favorite_team_points_for',
-                    # 'underdog_team_points_for',
-                    # 'favorite_team_points_against',
-                    # 'underdog_team_points_against',
+                    # 'favorite_team_over_rate',
+                    # 'underdog_team_over_rate',
+                    # 'favorite_team_total_points_ats',
+                    # 'underdog_team_total_points_ats',
+                    'favorite_team_points_for',
+                    'underdog_team_points_for',
+                    'favorite_team_points_against',
+                    'underdog_team_points_against',
                 ]
         },
             'over': {
@@ -120,17 +120,17 @@ class Model(Data):
                     # Win-rate ATS
                     'favorite_team_win_rate_ats',
                     'underdog_team_win_rate_ats',
-                    'favorite_team_margin_ats',
-                    'underdog_team_margin_ats',
+                    # 'favorite_team_margin_ats',
+                    # 'underdog_team_margin_ats',
                     # PF and PA
-                    'favorite_team_over_rate',
-                    'underdog_team_over_rate',
-                    'favorite_team_total_points_ats',
-                    'underdog_team_total_points_ats',
-                    # 'favorite_team_points_for',
-                    # 'underdog_team_points_for',
-                    # 'favorite_team_points_against',
-                    # 'underdog_team_points_against',
+                    # 'favorite_team_over_rate',
+                    # 'underdog_team_over_rate',
+                    # 'favorite_team_total_points_ats',
+                    # 'underdog_team_total_points_ats',
+                    'favorite_team_points_for',
+                    'underdog_team_points_for',
+                    'favorite_team_points_against',
+                    'underdog_team_points_against',
                 ]
             }
         }
